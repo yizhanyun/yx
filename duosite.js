f076a63405de87c772c4c88366cd7753c654358b
lanyusan <fqye@daima.co>
2021-02-27 15:38:08 +0800

    run duosite with shelljs

--- duosite.js
+++ duosite.js
@@ -1,19 +1,11 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process')
+const shell = require('shelljs')
+
 const env = process.argv[2]
 
 if (env !== 'prod' && env !== 'dev') {
   console.log('Wrong argument. \nPlease run duosite with dev or duosite prod')
   return
 }
-if (env === 'dev')
-  exec('yarn nodemon ./server.js', (err, stdout, stderr) => {
-    if (err) {
-      //some err occurred
-      console.error(err)
-    } else {
-      // the *entire* stdout and stderr (buffered)
-      console.log(`stdout: ${stdout}`)
-    }
-  })
+if (env === 'dev') shell.exec('yarn dev')
